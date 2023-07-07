fef3167ce873b9d56f14dbe993417b4bbe69a82b
pierreachkar1717 <pa27cufo@studserv.uni-leipzig.de>
2023-07-07 16:22:41 +0200

    filtering

---
+++ src/data_extraction/filtering.py
@@ -0,0 +1,53 @@
+""" 
+Filter out rows where the abstract does not contain the word ("artificial intelligence" or "intelligence" or "machine learning") and the word ("sustainability" or "sustainable") and the word ("energy" or "energies")
+"""
+import pandas as pd
+
+
+def filter_dataframe(df, source):
+    """
+    Filter out rows where the abstract does not contain the word ("artificial intelligence" or "intelligence" or "machine learning") and the word ("sustainability" or "sustainable") and the word ("energy" or "energies")
+    and save the filtered dataframe to a csv file
+    and print how many rows were filtered out
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        dataframe to filter
+    source : str
+        source of the dataframe
+
+    Returns
+    -------
+    filtered_df : pandas.DataFrame
+        filtered dataframe
+    """
+
+    # filtering out rows
+    mask1 = df["abstract"].str.contains(
+        "artificial intelligence|intelligence|intelligent agent|machine learning",
+        na=False,
+        case=False,
+    )
+    mask2 = df["abstract"].str.contains(
+        "sustainability|sustainable", na=False, case=False
+    )
+    mask3 = df["abstract"].str.contains("energy|energies", na=False, case=False)
+
+    filtered_df = df[mask1 & mask2 & mask3]
+
+    # print how many rows were filtered out
+    print(f"Filtered out {len(df) - len(filtered_df)} rows from {source} dataframe")
+    print(f"{len(filtered_df)} rows left in {source} dataframe")
+
+    filtered_df.to_csv(f"./data/raw_data/filtered_{source}_data.csv", index=False)
+
+    return filtered_df
+
+
+def main():
+    df = pd.read_csv("./data/raw_data/acm_data_with_abstract.csv")
+    filtered_df = filter_dataframe(df, "acm")
+
+
+if __name__ == "__main__":
+    main()
